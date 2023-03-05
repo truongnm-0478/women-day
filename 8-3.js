e2ef82e7eed81d2ede415ff6d4e45145dcaca87e
htnhcmus <huynhtrongnhanhcmus@gmail.com>
2023-03-06 00:02:58 +0700

    update 2

--- 8-3.js
+++ 8-3.js
@@ -1,8 +1,10 @@
 function appear() {
     document.querySelector(".topanimation").classList.add("appear")
     document.querySelector(".text").classList.add("appear")
+    document.querySelector(".click").classList.add("appear")
     document.querySelector(".title").classList.add("appear_text")
     document.querySelector(".background_blur").classList.add("appear_text")
     document.querySelector(".letter").classList.add("appear_text")
+    document.querySelector(".from").classList.add("appear_text")
 
 }
